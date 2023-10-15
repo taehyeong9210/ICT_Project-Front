5afd4d680e0caeccacc00727c26314fdaa320d78
taehyeong9210 <skytubu@naver.com>
2023-10-15 12:54:17 +0900

    로그인 화면

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from '.pages/Login';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          tes
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <div className="head">APP 이름</div>
+      <div className="content">
+        <div className="innerContent"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
