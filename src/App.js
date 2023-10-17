4cf2a1e593011bf2804e943cd901d3acb424ba5d
taehyeong9210 <skytubu@naver.com>
2023-10-17 22:16:58 +0900

    로그인

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import React from 'react';
+import Signup from './pages/Signup';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
